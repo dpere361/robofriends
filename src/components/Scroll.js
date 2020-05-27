4887b2b91db77e9ebed9f3db680ee884c118d655
Danny Perez <dpere361@fiu.edu>
2020-05-27 17:35:46 -0400

    Completed first iteration of app

---
+++ src/components/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
+
+// using children allows this component to wrap around other components 
+const Scroll = (props) => {
+    return (
+        <div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+            {props.children}
+        </div>
+    );
+
+};
+
+export default Scroll;
